826e661449d6e980762478742550a49d389864bf
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-08-01 08:22:44 -0300

    finish adm page: - add custom status (svg) - add all brazilian states

--- starwars/commerce/models.py
+++ starwars/commerce/models.py
@@ -12,9 +12,33 @@ class BaseModel(models.Model):
 
 class Address(models.Model):
     STATE_CHOICES = (
-        ("sp", "SP"),
-        ("pr", "PR"),
-        ("mg", "MG"),
+        ("AC", "AC"),
+        ("AL", "AL"),
+        ("AP", "AP"),
+        ("AM", "AM"),
+        ("BA", "BA"),
+        ("CE", "CE"),
+        ("DF", "DF"),
+        ("ES", "ES"),
+        ("GO", "GO"),
+        ("MA", "MA"),
+        ("MT", "MT"),
+        ("MS", "MS"),
+        ("MG", "MG"),
+        ("PA", "PA"),
+        ("PB", "PB"),
+        ("PR", "PR"),
+        ("PE", "PE"),
+        ("PI", "PI"),
+        ("RJ", "RJ"),
+        ("RN", "RN"),
+        ("RS", "RS"),
+        ("RO", "RO"),
+        ("RR", "RR"),
+        ("SC", "SC"),
+        ("SP", "SP"),
+        ("SE", "SE"),
+        ("TO", "TO"),
     )
 
     state = models.CharField(
